a15beaa240122b9be1b217a3f2527ddd5a892686
yukghvb <a96363877@gmail.com>
2025-03-26 19:21:04 +0000

    werr

--- src/pages/InsuranceDetails.tsx
+++ src/pages/InsuranceDetails.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { addData } from "../apis/firebase";
 
 type InsuranceType = "against-others" | "special" | "comprehensive";
