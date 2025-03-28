4ce94003179490b18a9ab029ff1ce2bf5376c9c5
yukghvb <a96363877@gmail.com>
2025-03-28 03:55:31 +0000

    asdAS

--- src/pages/OtpVerification.tsx
+++ src/pages/OtpVerification.tsx
@@ -311,7 +311,6 @@ export default function OtpVerification() {
         <STCModal
           isOpen={showSTCModal}
           onClose={(): void => {
-            setShowSTCModal(false);
           }}
         />
       )}
